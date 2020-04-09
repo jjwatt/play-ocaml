e09d590f54cb0841c87496a83efbc7fe68009970
Jesse Wattenbarger <jesse.j.wattenbarger@gmail.com>
2020-04-09 13:34:35 -0400

    Start of a sketch.

--- noboot-sketch/src/noboots.ml
+++ noboot-sketch/src/noboots.ml
@@ -1,2 +1,24 @@
 print_endline "Hello, world!"
 
+module DutFailureCommon = struct
+  type t = { post_code: string;
+             symptom_message: string;
+             symptom_label: string
+           }
+end
+
+module MoboLights = struct
+  type t = { front: string }
+end
+
+type seriallog =
+  | Present of bool
+  | Nothing
+
+(* Not quite right. The logs can be present and empty, present and non-empty or missing *)
+
+type prefectlog =
+  | Present of bool
+  | Nothing
+
+(* We can make custom types for mobo lights and post codes *)

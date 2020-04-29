193956e98cab599c65623891f3de4fdbdba58ff3
Jesse J. Wattenbarger <jesse.j.wattenbarger@gmail.com>
2020-04-29 15:27:32 -0400

    Add bubble sort impls to scratch file.

--- scratch/src/scratch.ml
+++ scratch/src/scratch.ml
@@ -68,3 +68,33 @@ let myrevs s =
   in aux s [] 0
 ;;
 
+(* bubblesort -- from rosettacode *)
+let rec bsort s =
+  let rec _bsort =
+    function
+    | x :: x2 :: xs when x > x2 ->
+      x2 :: _bsort (x::xs)
+    | x :: x2 :: xs ->
+      x :: _bsort (x2::xs)
+    | s -> s
+  in
+  let t = _bsort s in
+  if t = s then t
+  else bsort t
+;;
+
+(* bubblesort -- adapted from StandardML version on rosettacode *)
+(* Seems a little clearer on how it works *)
+(* TODO: Adapt the scheme fixpoint one to Ocaml *)
+let rec bubble_select =
+  function
+  | [] -> []
+  | [a] -> [a]
+  | a::b::xs -> if b < a
+    then b::(bubble_select(a::xs))
+    else a::(bubble_select(b::xs))
+let rec bs_bsort =
+  function
+  | [] -> []
+  | x::xs -> bubble_select (x::(bs_bsort xs))
+;;

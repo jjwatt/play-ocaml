cf8d0d6089441f85401c2e6ed6bf1e2153473a8f
Jesse J. Wattenbarger <jesse.j.wattenbarger@gmail.com>
2020-04-29 14:59:15 -0400

    Add a bunch of functions I'm playing with to scratch.

--- scratch/src/scratch.ml
+++ scratch/src/scratch.ml
@@ -10,3 +10,61 @@ let rec insert lst x =
 (* let insertion_sort = List.fold_left insert [];; *)
 (* Core.Std List.fold_left needs named args *)
 let insertion_sort = List.fold_left ~f:insert ~init:[]
+;;
+
+(* fold_left : ('a -> 'b -> 'a) -> 'a -> 'b list -> 'a *)
+let rec fold_left f a l =
+  match l with
+    [] -> a
+  | h::t -> fold_left f (f a h) t
+;;
+
+(* fold_right : ('a -> 'b -> 'b) -> 'a list -> 'b -> 'b *)
+let rec fold_right f l a =
+  match l with
+    [] -> a
+  | h::t -> f h (fold_right f t a)
+;;
+
+(* Reverse a list -- from 99 problems in ocaml *)
+let revl list =
+  let rec aux acc =
+    function
+    | [] -> acc
+    | h::t -> aux (h::acc) t in
+  aux [] list
+;;
+(* Reverse a list, simpler/definitional *)
+let rec revlA l =
+  match l with
+  | [] -> []
+  | h::t -> revlA t @ [h]
+;;
+(* NOTE: It looks so much like a fold_left, but I can't get it to work with fold_left *)
+
+let is_palindromel l =
+  l = revl l (* list is equal to revl list *)
+;;
+
+(* Reverse a string -- from rosettacode *)
+let rec revs_aux strin list index =
+  if List.length list = String.length strin
+  then String.concat ~sep:"" list
+  else revs_aux strin ((String.sub strin index 1)::list)(index + 1)
+
+let revs s = revs_aux s [] 0
+;;
+
+let is_palindromes s =
+  s = revs s
+;;
+
+(* Combining revs_s to one function *)
+let myrevs s =
+  let rec aux strin list index =
+    if List.length list = String.length strin
+    then String.concat ~sep:"" list
+    else aux strin ((String.sub strin index 1)::list)(index + 1)
+  in aux s [] 0
+;;
+

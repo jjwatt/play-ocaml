8fbfecdceba82ef5d2e2fed7aca0c0d4e3eb289e
Jesse J. Wattenbarger <jesse.j.wattenbarger@gmail.com>
2020-05-01 13:26:22 -0400

    Move sorting functions to a new sorts.ml

--- scratch/src/scratch.ml
+++ scratch/src/scratch.ml
@@ -1,17 +1,5 @@
 open Core
 
-let rec insert lst x =
-  match lst with
-  |  [] -> [x]
-  | y :: ys when x <= y -> x :: y :: ys
-  | y :: ys -> y :: insert ys x
-
-
-(* let insertion_sort = List.fold_left insert [];; *)
-(* Core.Std List.fold_left needs named args *)
-let insertion_sort = List.fold_left ~f:insert ~init:[]
-;;
-
 (* fold_left : ('a -> 'b -> 'a) -> 'a -> 'b list -> 'a *)
 let rec fold_left f a l =
   match l with
@@ -69,35 +57,4 @@ let myrevs s =
     then String.concat ~sep:"" list
     else aux strin ((String.sub strin index 1)::list)(index + 1)
   in aux s [] 0
-;;
-
-(* bubblesort -- from rosettacode *)
-let rec bsort s =
-  let rec _bsort =
-    function
-    | x :: x2 :: xs when x > x2 ->
-      x2 :: _bsort (x::xs)
-    | x :: x2 :: xs ->
-      x :: _bsort (x2::xs)
-    | s -> s
-  in
-  let t = _bsort s in
-  if t = s then t
-  else bsort t
-;;
-
-(* bubblesort -- adapted from StandardML version on rosettacode *)
-(* Seems a little clearer on how it works *)
-(* TODO: Adapt the scheme fixpoint one to Ocaml *)
-let rec bubble_select =
-  function
-  | [] -> []
-  | [a] -> [a]
-  | a::b::xs -> if b < a
-    then b::(bubble_select(a::xs))
-    else a::(bubble_select(b::xs))
-let rec bs_bsort =
-  function
-  | [] -> []
-  | x::xs -> bubble_select (x::(bs_bsort xs))
 ;;

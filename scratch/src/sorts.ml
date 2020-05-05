e4d0029475609113e4f781e23a4dca30b91c9eda
Jesse Wattenbarger <jesse.j.wattenbarger@gmail.com>
2020-05-05 14:00:13 -0400

    Hack (#14)
    * Notes on nix and opam.
    
    * Add imperative array insertion sort.

--- scratch/src/sorts.ml
+++ scratch/src/sorts.ml
@@ -12,6 +12,19 @@ let rec insert lst x =
 let insertion_sort = List.fold_left ~f:insert ~init:[]
 ;;
 
+(* imperative insertion sort on arrays -- from Ocaml Manual *)
+let imp_insertion_sort a =
+  for i = 1 to Array.length a - 1 do
+    let val_i = a.(i) in
+    let j = ref i in
+    while !j > 0 && val_i < a.(!j - 1) do
+      a.(!j) <- a.(!j - 1);
+      j := !j - 1
+    done;
+    a.(!j) <- val_i
+  done
+;;
+
 (* bubblesort -- from rosettacode *)
 let rec bsort s =
   let rec _bsort =

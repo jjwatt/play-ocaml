d404d1ba59018b556c175815db8c65d27e57267b
Jesse Wattenbarger <jwatt@broken.watch>
2020-08-07 14:21:01 -0400

    Add contents from Getting Started with F#

--- fsharp/FSNetCore/src/App/Program.fs
+++ fsharp/FSNetCore/src/App/Program.fs
@@ -1,8 +1,13 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Library
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+	printf "Nice command-line arguments! Here's what JSON.NET has to say about them:"
+	argv
+	|> Array.map getJsonNetJson
+	|> Array.iter (printfn "%s")
+	0 // return an integer exit code
+

8d138403b3ab8cb0c5e15f27c35fa5878320e48e
Jesse Wattenbarger <jwatt@broken.watch>
2020-08-07 15:25:13 -0400

    Fix bugs in fsharp formatting

--- fsharp/FSNetCore/src/App/Program.fs
+++ fsharp/FSNetCore/src/App/Program.fs
@@ -5,9 +5,9 @@ open Library
 
 [<EntryPoint>]
 let main argv =
-	printf "Nice command-line arguments! Here's what JSON.NET has to say about them:"
-	argv
-	|> Array.map getJsonNetJson
-	|> Array.iter (printfn "%s")
-	0 // return an integer exit code
+    printf "Nice command-line arguments! Here's what JSON.NET has to say about them:"
+    argv
+    |> Array.map getJsonNetJson
+    |> Array.iter (printfn "%s")
 
+    0

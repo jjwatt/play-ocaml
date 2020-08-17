eb0773d355da0de06913bf298069532694eba897
Jesse Wattenbarger <jwatt@broken.watch>
2020-08-17 16:45:51 -0400

    Using Option type in fsharp minrefills
    But, I still don't have it working for impossible journeys.

--- fsharp/alg/minrefills.fs
+++ fsharp/alg/minrefills.fs
@@ -5,18 +5,17 @@ let minrefills distance tank stations =
         nextStop - lastRefill >= tank
     let allStops = stations @ [distance]
     let rec aux stations last acc =
-        if (List.head(stations) - last) - tank < 0 then None
-        else
-        match stations with
-            | [] -> Some(acc)
-            | [curr] -> if needStop last curr
-                          then Some(acc + 1)
-                          else Some(acc)
-            | curr :: tail -> if needStop last curr
-                                then aux tail curr (Some(acc + 1))
-                                else aux tail last (Some(acc))
+        match stations, acc with
+            | _, None -> None
+            | [], Some(acc) -> Some(acc)
+            | [curr], Some(acc) -> if needStop last curr
+                                   then Some(acc + 1)
+                                   else Some(acc)
+            | curr :: tail, Some(acc) -> if needStop last curr
+                                         then aux tail curr (Some(acc + 1))
+                                         else aux tail last (Some(acc))
             in
-     aux allStops 0 0
+     aux allStops 0 (Some(0))
 
 // Do you have enough to get to the next stop?
 // Yes: Go to the next stop, subtracting fuel

ff8705e05b1dff3814ddd354fd4a7f1644e3f60b
Jesse Wattenbarger <jesse.j.wattenbarger@gmail.com>
2020-11-03 21:59:43 -0500

    Modules structs and functors in ocaml scratch

---
+++ ocaml/scratch/.dir-locals.el
@@ -0,0 +1,6 @@
+((nil .
+      ((eval . (add-hook 'tuareg-mode-hook #'(lambda() (setq mode-name "🐫"))))))
+ (tuareg-mode .
+              ((merlin-command . "ocamlmerlin")
+               (utop-command . "utop -emacs -init ~/.ocamlinit")
+               (eval . (prettify-symbols-mode)))))
